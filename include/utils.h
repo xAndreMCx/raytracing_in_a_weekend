e2092ef2ee68c54ccdc4f40619f3d2af028e926b
Andre du Toit <xandremcx@gmail.com>
2025-12-19 23:23:45 +0200

    add function for multithreading

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <stdint.h>
+
 /**
  * @brief The value of pi.
  */
 #define RT_PI 3.1415926535897932385
+
+/**
+ * @brief Normalization constant for 32-bit random integers (2^32).
+ */
+#define RT_UINT32_MAX 4294967296
 
 /**
  * @brief Converts degrees to radians.
@@ -13,7 +20,7 @@
  * @param x The angle in degrees.
  * @return The angle in radians.
  */
-#define DEG_TO_RAD(x) ((x) * (RT_PI / 180))
+#define DEG_TO_RAD(x) ((x) * (RT_PI / 180.0))
 
 /**
  * @brief Converts radians to degrees.
@@ -23,12 +30,31 @@
  * @param x The angle in radians.
  * @return The angle in degrees.
  */
-#define RAD_TO_DEG(x) ((x) * (180 / RT_PI))
+#define RAD_TO_DEG(x) ((x) * (180.0 / RT_PI))
+
+/**
+ * @brief Thread-local random number generator state.
+ *
+ * Each thread has its own private instance of this variable to ensure
+ * thread-safe random number generation without global locks.
+ */
+extern _Thread_local unsigned int rng_state;
+
+/**
+ * @brief Seeds the thread-local RNG.
+ *
+ * This should be called once at the start of each worker thread 
+ * using a unique value (like the thread ID).
+ *
+ * @param seed The initial seed value.
+ */
+void seed_thread_rng(unsigned int seed);
 
 /**
  * @brief Generates a random double between min and max.
  *
  * This function generates a random double value in the range [min, max).
+ * It uses the thread-local rng_state.
  *
  * @param min The minimum value.
  * @param max The maximum value.
--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,34 @@
 #include "utils.h"
-
 #include <math.h>
-#include <stdlib.h>
-#include <time.h>
 
-double rand_double(double min, double max) { return min + (max - min) * (rand() / (RAND_MAX + 1.0)); }
+_Thread_local unsigned int rng_state = 123456789;
 
-double linear_to_gamma(double linear) { return pow(linear, 1.0 / 2.2); }
+/**
+ * @brief Internal Xorshift32 algorithm.
+ *
+ * Operates on the calling thread's private rng_state.
+ *
+ * @return A pseudo-random 32-bit unsigned integer.
+ */
+static unsigned int xorshift32(void) {
+  rng_state ^= rng_state << 13;
+  rng_state ^= rng_state >> 17;
+  rng_state ^= rng_state << 5;
+  return rng_state;
+}
+
+void seed_thread_rng(unsigned int seed) {
+  if (seed != 0) {
+    rng_state = seed;
+  }
+}
+
+double rand_double(double min, double max) {
+  double random_unit = xorshift32() / (float)RT_UINT32_MAX;
+  return min + (max - min) * random_unit;
+}
+
+double linear_to_gamma(double linear) {
+  // Standard Gamma 2.2 correction
+  return (linear > 0) ? pow(linear, 1.0 / 2.2) : 0;
+}
